2233bb9f0991ebfc81c750ba00ff1c04a91de41b
OniUnderscore <ellysgudger@gmail.com>
2024-07-16 13:17:10 +0100

    Seperated out Event Handler logic

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("fs");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+//Command Handler
+
 client.commands = new Collection();
 
 const commandFolders = fs.readdirSync("./commands");
@@ -25,35 +27,19 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+//Event Handler
+
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith("js"));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
 client.login(process.env.CLIENT_TOKEN);
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    if (interaction.replied || interaction.deferred) {
-      await interaction.followUp({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    } else {
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
-    }
-  }
-});
